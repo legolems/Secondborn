bbe0e17d262d48d09f41689e24e08e301af5e89b
Kurkos <41972994+Kurkos@users.noreply.github.com>
2018-08-14 12:54:32 +0300

    Java Problem
    Java Problem

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+<?php
+
+if (isset($_GET["param"])) {
+		if ($_GET["param"] == "test") {
+			echo "THIS IS FUCKING GREATE TEST";
+	} else {
+			echo "SELECT DIFFERENT PARAM";
+	}
+} else {
+	echo "GO FUCK YOUR SELF";
+}
+
+?>
---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('test')
+
+
+function showPopup () {
+	var popup = document.getElementById('popup');
+	popup.classList.add('show');
+}
+
+function hidePopup () {
+	var popup = document.getElementById('popup');
+	popup.classList.remove('show');
+}
